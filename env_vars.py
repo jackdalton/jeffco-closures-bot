2b0b2464bae8a22207cf3f7c7993414c9bee7389
Jack Dalton <jack@jackdalton.org>
2019-02-08 15:16:25 +0000

    Oops

---
+++ env_vars.py
@@ -0,0 +1,7 @@
+import os
+
+consumer_key = os.environ.get("consumer_key", None)
+consumer_secret = os.environ.get("consumer_secret", None)
+access_token = os.environ.get("access_token", None)
+access_token_secret = os.environ.get("access_token_secret", None)
+account = os.environ.get("account", None)
